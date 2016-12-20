2a124843c91b20563490c193ca5bfcfb7cb2243c
ackzell <amartinez@itexico.net>
2016-12-19 22:33:27 -0600

    Routing app working with Vue 2.1.6

---
+++ client/main.js
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+import Vue from 'vue';
+import router from '/imports/routes';
+
+import AppLayout from '/imports/ui/AppLayout.vue';
+
+// App start
+Meteor.startup(() => {
+  // Start the router
+  new Vue({
+    router: router.start(),
+    render: h => h(AppLayout),
+  }).$mount('app');
+});
---
+++ imports/ui/Page2.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <div class="page1">
+    <h1>Page 2</h1>
+    <p>You are in the second page</p>
+  </div>
+</template>
